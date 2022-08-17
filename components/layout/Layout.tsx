d6ce5fb80f4641d5513ace8975f75146ac10ce4b
tboba <tymoteusz.boba@gmail.com>
2022-08-17 12:07:05 +0200

    Fix problem with no background on scroll

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -8,6 +8,13 @@ const Layout: React.FC<{children: React.ReactNode}> = ({children}) => {
         backgroundColor='#f2f4f7'
     >
         {children}
+        <style global jsx>{`
+              html, body, div#__next {
+                height: 100vh;
+                min-height: 100vh;
+                background-color: #f2f4f7;
+              }
+            `}</style>
     </Flex>
 }
 
