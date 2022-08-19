84692a1afa57243c2e34b3d4bec748d3a537ab68
tboba <tymoteusz.boba@gmail.com>
2022-08-19 13:40:05 +0200

    Fix a problem with content width when events title is not long enough

--- components/layout/CenteredSubLayout.tsx
+++ components/layout/CenteredSubLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Flex} from "@chakra-ui/react";
 
 const CenteredSubLayout: React.FC<{ children: React.ReactNode }> = ({children}) => {
-    return <Flex flexDirection='column' alignItems='center'>
+    return <Flex flexDirection='column' alignItems='center' alignContent='center' w='100%' minW='100%'>
         {children}
     </Flex>
 }
