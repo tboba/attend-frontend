91506cf7fd80b85af18bc8ee18b07071b4dd515d
tboba <tymoteusz.boba@gmail.com>
2022-08-25 01:12:20 +0200

    Make Attend Logo clickable

--- components/ui/header/MainHeader.tsx
+++ components/ui/header/MainHeader.tsx
@@ -1,11 +1,21 @@
 import {Box, Flex, Text} from "@chakra-ui/react";
 import MainNavigation from "./MainNavigation";
+import {useRouter} from "next/router";
 
 const MainHeader = () => {
+    const router = useRouter();
+
+    const handleLogoClick = async () => {
+        await router.push('/');
+    }
+
     return <Box as='header' alignSelf='stretch'>
         <Flex w='100%' h='5rem' alignItems='center' justifyContent='space-between' padding='0 10%'>
-            <Text fontFamily='Acme' fontSize='2em'>Attend</Text>
-            <MainNavigation />
+            <Text fontFamily='Acme'
+                  fontSize='2em'
+                  cursor='pointer'
+                  onClick={handleLogoClick}>Attend</Text>
+            <MainNavigation/>
         </Flex>
     </Box>
 }
